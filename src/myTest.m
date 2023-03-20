0805770e2e89aa9c72bce3e85d42c9218b8ca57a
hPixal <nahuel.j.gareis@protonmail.com>
2023-03-20 10:30:31 -0300

    Trabajo Práctico 1 Ejercicios que no son del aula

---
+++ src/myTest.m
@@ -0,0 +1,106 @@
+printf("this is a octave hello world \n \n");
+% CN2023_TP0
+% Ejercicio 1
+
+printf("Ejercicio 1 \n \n");
+
+a1 = 5^2 - 1/(2 ^ 3) - sqrt(3^2 + (2*2)^2);
+a1
+b1 = sin(pi/6) - atan(1/2);
+b1
+c1 = log(3 + 1/5) - e^2;
+c1
+
+% Ejercicio 2
+
+printf("Ejercicio 2 \n \n");
+
+function [y] = myFunction(x)
+    y = sin(2*x) / ( x * (x + 1));
+end;
+
+myFunction((-4))
+myFunction((-pi/8))
+myFunction(sqrt(2)/4)
+myFunction(pi/2)
+myFunction(9*pi/5)
+myFunction(0)
+myFunction(-1)
+
+% Ejercicio 3
+
+printf("Ejercicio 3 \n \n");
+
+a3 = [ 1 2 3 -4 ]
+b3 = [ 1 2 3 -4 ]'
+c3 = -2.5:0.5:1
+d3 = ( -2.5:0.5:1 )'
+e3 = [-3:2:4]
+
+% Ejercicio 4
+
+printf("Ejercicio 4 \n \n");
+
+x = [ -0.5 7 2 ];
+z = [ 2 -1 3 ]';
+
+2*x
+y = x-1
+
+x.*y
+
+y.^2
+
+x' + 5*z
+
+x+5*z
+
+y-z'/3
+
+v = [x,y]
+
+v(2:5)
+
+v(5:6)+(z(1:2))'
+
+w = [x;y]
+
+% Ejercicio 5
+
+printf("Ejercicio 5 \n \n");
+
+p = [ 2 3 4 7 ]
+
+polyout(p,'x');
+x = 3;
+polyval(v,x)
+roots(p)
+
+% Ejercicio 6
+
+% Ejercicio 7
+
+printf("Ejercicio 7 \n \n");
+
+clearvars
+
+m = 5
+n = 4*m+1
+x = linspace(0,1,n)
+y = zeros(1,n)
+a = x(1:m+1)
+y(1:m+1) = sin(2*pi*a)
+y(2*m+1:-1:m+2) = y(1:m)
+% y(2*m+2:n) = -y(2:2*m);
+
+% Ejercicio 8
+
+printf("Ejercicio 8 \n \n ")
+
+function [y1] = myFunction2(x1)
+    y1 = (((x1 + 1) / (x1 ^ 2 + x1 +1 )) ^ 6) * (cos(x1) + 3)
+end;
+
+hold on
+plot(x,y)
+hold off
