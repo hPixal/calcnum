d46c4ee2da9ef924b179d169591c64dcd2e4b42a
hPixal <nahuel.j.gareis@protonmail.com>
2023-05-02 08:58:34 -0300

    moreFixing

--- src/RootFinding/secant.m
+++ src/RootFinding/secant.m
@@ -1,18 +1,24 @@
 function [fP,it,t] = secant(f_x, p0 , p1, tol, maxIt)
     tic();
-    it = 2; f_p0 = p0; f_p1 = p1;
+    it = 2;
+    f_p0 = f_x(p0);
+    f_p1 = f_x(p1);
+
     while it < maxIt
-        fP = p1 - f_p1*(p1 - p2)/(f_p1 - f_p0);
+        fP = p1 - f_p1*(p1 - p0)/(f_p1 - f_p0);
 
         mResidue = abs(fP - p1);
         if mResidue < tol
             break;
         endif
 
-        p0 = p1 ; f_p0 = f_p1;
+        p0 = p1 ; f_p0 = f_p1; p1 = fP; q1 = f_x(fP);
         f_p1 = f_x(fP);
 
         it = it+1;
     endwhile
     t = toc();
+
+    plotFunction(f_x,p0-5,p1+5,100,1)
+    plotDots([ f_x(fP) fP ], 1 );
 end
